2a39e8a2b1260c5251ea61a2a3146b55679f77f3
OldDream <huangyn15@qq.com>
2020-11-18 22:38:09 +0800

    init rollup

---
+++ README.md
@@ -0,0 +1,6 @@
+# crude-copy
+
+A 🚀 light-weight tool focus on copy string to clipboard.
+
+⚠️ Only support modern browser.
+
---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('@rollup/plugin-node-resolve');
+const pkg = require('./package.json');
+
+const extensions = ['.js', '.ts'];
+
+const resolve = function(...args) {
+  return path.resolve(__dirname, ...args);
+};
+
+module.exports = {
+  input: resolve('./src/index.ts'),
+  output: {
+    file: resolve('./', pkg.main), // 为了项目的统一性，这里读取 package.json 中的配置项
+    format: 'esm',
+  },
+  plugins: [
+    nodeResolve({
+      extensions,
+      modulesOnly: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions,
+    }),
+  ],
+};

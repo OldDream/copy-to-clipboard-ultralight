1186d65ebc86d3fcb95878650ac41a202cc2df59
OldDream <huangyn15@qq.com>
2020-11-20 12:36:57 +0800

    add：optimize config

--- rollup.config.js
+++ rollup.config.js
@@ -4,33 +4,34 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import dts from "rollup-plugin-dts";
 import pkg from './package.json';
 
-const extensions = ['.js', '.ts'];
+const extensions = ['.ts'];
 
 const resolve = function (...args) {
   return path.resolve(__dirname, ...args);
 };
 
-const config = [{
+const config = [
+{ // 编译typescript
   input: resolve('./src/index.ts'),
   output: {
-    file: resolve('./', pkg.main), // 为了项目的统一性，这里读取 package.json 中的配置项
+    file: resolve('./', pkg.main), // 读取 package.json 中的main作为入口。
     format: 'umd',
     name: 'ultralightCopy',
   },
   plugins: [
     nodeResolve({
-      extensions,
-      modulesOnly: true,
+      extensions
     }),
     babel({
       exclude: 'node_modules/**',
       extensions,
     })
   ],
-}, {
+}, 
+{ // 生成 .d.ts 类型声明文件
   input: resolve('./src/index.ts'),
   output: {
-    file: resolve('./', pkg.types), // 为了项目的统一性，这里读取 package.json 中的配置项
+    file: resolve('./', pkg.types), 
     format: 'es',
   },
   plugins: [dts()],
